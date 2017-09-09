58757cfc2247aef9fe1e4963bf77b16b507d54f2
valle vali <valle vali>
2017-09-09 11:35:40 +0200

    Add project files.

---
+++ TstStreamAuctioneer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TstStreamAuctioneer.MvcApplication" Language="C#" %>
---
+++ TstStreamAuctioneer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TstStreamAuctioneer.Startup))]
+namespace TstStreamAuctioneer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
