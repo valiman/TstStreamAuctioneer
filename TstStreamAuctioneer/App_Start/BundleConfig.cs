eb4a76d852a90e8a3efc75ab3e91f604dff0cd14
valle <valiman@live.se>
2017-09-18 16:04:03 +0200

    TestFest

--- TstStreamAuctioneer/App_Start/BundleConfig.cs
+++ TstStreamAuctioneer/App_Start/BundleConfig.cs
@@ -5,6 +5,7 @@ namespace TstStreamAuctioneer
 {
     public class BundleConfig
     {
+        //Testing..
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
